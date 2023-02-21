37f7d743155c642500b15dc544813077920d7e1c
yxxnghwan <younghwan960@gmail.com>
2023-02-22 00:12:58 +0900

    init

---
+++ src/main/java/com/example/springbatchtutorial/SpringbatchtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchtutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbatchtutorial/SpringbatchtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
